6949fb56191eb18754365dad8c30bf1db20784b0
BoxesOfBush <qatuhhr@gmail.com>
2020-12-04 10:54:15 +0200

    add Function_and_Recursion_6

---
+++ Function and Recursion/Function_and_Recursion_6.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+bool IsPointInArea(double x, double y) {
+    double in_circle = 2 * 2 >= abs(x + 1) * abs(x + 1) + abs(y - 1) * abs(y - 1);
+    double above_line1 = y >= 2 * x + 2;
+    double above_line2 = y >= -x;
+    double below_line1 = y <= 2 * x + 2;
+    double below_line2 = y <= -x;
+    double on_circle = 2 * 2 == abs(x + 1) * abs(x + 1) + abs(y - 1) * abs(y - 1);
+
+    return in_circle && above_line1 && above_line2 || (on_circle || not in_circle) && below_line1 && below_line2;
+}
+
+int main() {
+    double  x, y;
+    std::cin >> x >> y;
+    if (IsPointInArea(x, y)) std::cout << "YES";
+    else std::cout << "NO";
+}
