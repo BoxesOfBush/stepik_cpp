dbf26b6fc1a0f8df42e1565a610e16dba3b3acb7
BoxesOfBush <qatuhhr@gmail.com>
2021-01-10 13:49:13 +0200

    add Ravesli

--- Ravesli/ravesli_39_1/main.cpp
+++ Ravesli/ravesli_39_1/main.cpp
@@ -4,17 +4,19 @@ bool isEven(int x){
     return(x % 2) == 0;
 }
 
-
-
-int main(){
-
+int getNumberfromUser(){
     std::cout << "Enter the number: ";
     int x;
     std::cin >> x;
-    if(isEven(x)){
-        std::cout << "Number " << x << "is even\n";
+    return x;
+}
+
+int main(){
+
+    if( int x = getNumberfromUser(); isEven(x) ){
+        std::cout << "Number " << x << " is even\n";
     }
     else{
-        std::cout << "Number " << x << "is not even\n";
+        std::cout << "Number " << x << " is not even\n";
     }
 }
